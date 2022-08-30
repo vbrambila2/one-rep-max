717b822f82ffcba23fca57146abf4d1da304e267
Victor Brambila <victorbrambila22@gmail.com>
2022-08-30 13:36:54 -0700

    added dotenv

--- server/index.js
+++ server/index.js
@@ -2,22 +2,23 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import movementRoutes from './routes/movements.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/movements', movementRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://vicbram:Placer11@cluster0.z0e85.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
     res.send('Your one rep maxes, together');
 });
 
-mongoose.connect( CONNECTION_URL )
+mongoose.connect( process.env.CONNECTION_URL )
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(error.message));
